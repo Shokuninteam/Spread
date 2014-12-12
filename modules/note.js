dc2bb6805a971e7dc9fa3ec90f08456246d12e90
Frank Bassard <f.bassard@gmail.com>
2014-12-12 20:55:09 +1300

    Basic models

---
+++ modules/note.js
@@ -0,0 +1,8 @@
+function Note(id, user, date, content, tags, spread) {
+  this.id = id;
+  this.user = user;
+  this.date = date;
+  this.content = content;
+  this.tags = tags;
+  this.spread = spread;
+}
---
+++ modules/user.js
@@ -0,0 +1,12 @@
+function User(id, nickname, mail, pwd, avatar, pos, favs, spreaded, history, settings) {
+  this.id = id;
+  this.nickname = nickname;
+  this.mail = mail;
+  this.pwd = pwd;
+  this.avatar = avatar;
+  this.pos = pos;
+  this.pos = favs;
+  this.pos = spreaded;
+  this.pos = history;
+  this.pos = settings;
+}
