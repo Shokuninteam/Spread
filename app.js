023e8894f0e7242a63b26831a837f7cb68e1d0de
frank bassard <f.bassard@gmail.com>
2014-12-12 20:27:08 +1300

    101 REST urls & mongo base test

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var http = require('http');
 var path = require('path');
 
 var user = require('./routes/user');
+var note = require('./routes/note');
+var mongo = require('./modules/mongo');
 
 var app = express();
 
@@ -25,7 +27,25 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//index
 app.get('/', user.list);
+//text
+app.get('/test', mongo.test);
+//API : user
+app.get('/users/id', user.getUser);
+app.get('/users/id', user.getUser);
+app.post('/users', user.createUser);
+app.put('/users/id', user.modifyUser);
+app.delete('/users/id', user.deleteUser);
+//API : note
+app.get('/notes/id', note.getNote);
+app.post('/notes', note.createNote);
+app.get('/users/id/notes/favoris', note.getFavs);
+app.get('/users/id/notes/history', note.gethistory);
+app.get('/users/id/notes/spreaded', note.gethostory);
+app.get('/notes/id/users/position', note.getUsersPositions);
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Port ' + app.get('port'));
