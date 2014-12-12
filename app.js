56e104e0f3c97d18951e6e61d40ab1cf9319e5d6
mohamedMok <Mohamed@MacBook-Air-de-Mohamed.local>
2014-12-12 22:52:09 +0100

    update 3.X to 4.X

--- app.js
+++ app.js
@@ -6,25 +6,38 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorhandler = require('errorhandler');
 
 var user = require('./routes/user');
 var note = require('./routes/note');
-var mongo = require('./modules/mongo');
+var mongo = require('./modules/services/mongo');
 
 var app = express();
 
+
+
+
+
+
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+app.use(morgan('dev'));
+//app.use(express.json());
+app.use(bodyParser.json());
+//app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.methodOverride());
+app.use(methodOverride('X-HTTP-Method-Override'));
+//app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorhandler());
 }
 
 //test
